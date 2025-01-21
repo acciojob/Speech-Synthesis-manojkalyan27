b7ad1fe7fcc740bac08e15c3e5ac8a73b0c544e9
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2025-01-21 15:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-// Your script here.
+
+  const voicesDropdown = document.querySelector('[name="voice"]');
+  const speakButton = document.querySelector('#speak');
+  const stopButton = document.querySelector('#stop');
+ 
+
+  const msg = new SpeechSynthesisUtterance();
+  let voices = [];
+
+  //Your code goes here
+    window.speechSynthesis.onvoiceschanged = () => {
+
+        voices = window.speechSynthesis.getVoices();   
+
+        voices.forEach((voice,i) => {
+            voicesDropdown.options[i] = new Option(voice.name,i) // creates new option
+        }
+        )
+    }
+
+    voicesDropdown.addEventListener("change", 
+        () =>  msg.voice = voices[voicesDropdown.value] //voice will be changed to dropdown value
+    )
+
+
+	speakButton.addEventListener("click",()=>{
+        //Below .text, .rate, .pitch ,.speak are given inbuilt   
+        msg.text = document.querySelector('textarea').value;
+        msg.rate = document.querySelector('[name="rate"]').value;
+        msg.pitch = document.querySelector('[ name="pitch"]').value;
+        window.speechSynthesis.speak(msg);
+    })
+
+    stopButton.addEventListener("click",()=>{
+        window.speechSynthesis.cancel();
+        //tops being spoken immediately
+})
